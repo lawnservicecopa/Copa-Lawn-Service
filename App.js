5433397da1aebc05b062ac03738c60ad5b7aa675
lawnservicecopa <lawnservicecopa@gmail.com>
2025-10-04 11:15:02 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import LawnCareLandingPage from "./LawnCareLandingPage";
+
+function App() {
+  return <LawnCareLandingPage />;
+}
+
+export default App;
---
+++ LawnCareLandingPage.js
@@ -0,0 +1,26 @@
+import React from "react";
+import {
+  MapPin,
+  Phone,
+  Mail,
+  Facebook,
+  Instagram,
+  Leaf,
+  Scissors,
+  Wind,
+  Droplet,
+  Trash2,
+  Trees,
+  Flower2,
+  Gift,
+  Sparkles,
+  Star,
+} from "lucide-react";
+import logo from "./copa-logo.png";
+
+export default function LawnCareLandingPage() {
+  // User's full code from above (kept intact)
+  return (
+    <div>Landing Page Placeholder</div>
+  );
+}
